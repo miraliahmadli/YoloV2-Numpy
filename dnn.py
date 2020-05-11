20e15ee8f458f8e123e9a54dbda6a7d8639dcdec
Mirali <miraliahmadli@gmail.com>
2020-05-11 03:49:41 +0000

    Some debugging

--- dnn.py
+++ dnn.py
@@ -110,34 +110,35 @@ class Conv2D(DnnNode):
         self.filter_height, self.filter_width, self.kernel_in_channels, self.out_channels = kernel.shape
 
         assert self.in_channels == self.kernel_in_channels, \
-        f"Shape of filters must be same to number of input channels, {self.kernel_in_channels} is not equal to {self.in_channels}"
+        "Shape of filters must be same to number of input channels, %d is not equal to %d" % (self.kernel_in_channels, self.in_channels)
 
         assert padding in ["SAME", "VALID"], \
-        f"Invalid padding name: {padding}, should be either SAME or VALID"
+        "Invalid padding name: %s, should be either SAME or VALID" % padding
 
         self.name = name
         print(self.name)
         self.kernel = kernel
         self.padding = (padding == "SAME")
-        self.strides = strides.shape
+        self.strides = strides
 
         self.pad_h = 0
         self.pad_w = 0
         if self.padding:
             # ((s-1) * x + k -s)/ 2
-            self.pad_h = (self.strides[1] - 1) * self.in_height + self.filter_height) - self.strides[1]) // 2
-            self.pad_w = (self.strides[2] - 1) * self.in_width + self.filter_width) - self.strides[2]) // 2
+            self.pad_h = ((self.strides[1] - 1) * self.in_height + self.filter_height - self.strides[1]) // 2
+            self.pad_w = ((self.strides[2] - 1) * self.in_width + self.filter_width - self.strides[2]) // 2
         
-        self.output_height  = (self.in_height - self.filter_height + 2 * self.pad_h) / self.strides[1] + 1
-        self.output_width   = (self.in_width - self.filter_width + 2 * self.pad_w) / self.strides[2] + 1
+        self.output_height  = int((self.in_height - self.filter_height + 2 * self.pad_h) / self.strides[1] + 1)
+        self.output_width   = int((self.in_width - self.filter_width + 2 * self.pad_w) / self.strides[2] + 1)
         self.result = np.zeros((self.batch, self.output_height, self.output_width, self.out_channels))
+        self.out_shape = self.result.shape
 
     def run(self):
         def multi(b, i, j, k):
             for h in range(self.filter_height):
                 for w in range(self.filter_width):
                     for q in range(self.in_channels):
-                        self.result[b, i, j, k] +=
+                        self.result[b, i, j, k] = \
                             (self.in_node.result[b, self.strides[1] * i + h, self.strides[2] * j + w, q] * 
                                 self.kernel[h, w, q, k])
             # average
@@ -152,14 +153,14 @@ class Conv2D(DnnNode):
                         for h in range(self.filter_height):
                             for w in range(self.filter_width):
                                 for q in range(self.in_channels):
-                                    self.result[b, i, j, k] +=
+                                    self.result[b, i, j, k] = \
                                         (self.in_node.result[b, self.strides[1] * i + h, self.strides[2] * j + w, q] * 
                                             self.kernel[h, w, q, k])
                         
                         # average
                         self.result[b, i, j, k] /= (self.filter_width * self.filter_height * self.in_channels)
 
-        self.out_shape = self.result.shape
+        
 
         # output[b, i, j, k] =
         #     sum_{di, dj, q} input[b, strides[1] * i + di, strides[2] * j + dj, q]
@@ -170,7 +171,7 @@ class BiasAdd(DnnNode):
         self.prev_res =  self.in_node.result
         self.batch, self.in_height, self.in_width, self.in_channels = in_node.out_shape
         assert self.in_channels == biases.shape[0], \
-        f"Shape of biases must be equal to number of input channels, {biases.shape[0]} is not equal to {self.in_channels}"
+        "Shape of biases must be equal to number of input channels, %d is not equal to %d" % (biases.shape[0], self.in_channels)
 
         self.biases = biases
         self.name = name
@@ -194,10 +195,10 @@ class MaxPool2D(DnnNode):
         self.batch, self.in_height, self.in_width, self.in_channels = in_node.out_shape
         self.out_channels = self.in_channels
         assert padding in ["SAME", "VALID"], \
-        f"Invalid padding name: {padding}, should be either SAME or VALID" 
+        "Invalid padding name: %s, should be either SAME or VALID" % padding
 
-        self.strides = strides.shape
-        self.ksize = ksizes.shape
+        self.strides = strides
+        self.ksize = ksize
         self.name = name
         print(self.name)
         self.in_node = in_node
@@ -208,27 +209,29 @@ class MaxPool2D(DnnNode):
         self.pad_w = 0
         if self.padding:
             # ((s-1) * x + k -s)/ 2
-            self.pad_h = (self.strides[1] - 1) * self.in_height + self.ksize[1]) - self.strides[1]) // 2
-            self.pad_w = (self.strides[2] - 1) * self.in_width + self.ksize[2]) - self.strides[2]) // 2
-
-            self.prev_res = np.zeros(self.in_node.out_shape)
+            self.pad_h = ((self.strides[1] - 1) * self.in_height + self.ksize[1] - self.strides[1]) // 2
+            self.pad_w = ((self.strides[2] - 1) * self.in_width + self.ksize[2] - self.strides[2]) // 2
+            self.prev_res = np.zeros((self.batch, self.in_height + 2*self.pad_h, self.in_width + 2*self.pad_w, self.in_channels))
             prev_copy = np.copy(self.in_node.result)
             self.prev_res[:, self.pad_h : -self.pad_h, self.pad_w : -self.pad_w, :] = prev_copy
         else:
             self.prev_res = np.copy(self.in_node.result)
         
-        self.output_height  = (self.in_height - self.ksize[1] + 2 * self.pad_h) / self.strides[1] + 1
-        self.output_width   = (self.in_width - self.ksize[2] + 2 * self.pad_w) / self.strides[2] + 1
+        self.output_height  = int((self.in_height - self.ksize[1] + 2 * self.pad_h) / self.strides[1] + 1)
+        self.output_width   = int((self.in_width - self.ksize[2] + 2 * self.pad_w) / self.strides[2] + 1)
         self.result = np.zeros((self.batch, self.output_height, self.output_width, self.out_channels))
         self.out_shape = self.result.shape
         
     def run(self):
         def get_max_from_window(b, x, y, c):
-            return np.argmax(self.prev_res[ b, 
-                                            x : min(x + self.ksize, self.in_height), 
-                                            y : min(y + self.ksize, self.in_width), 
-                                            c], 
-                             axis = None)
+            # window = self.prev_res[ b, x : min(x + self.ksize, self.in_height), 
+            #                         y : min(y + self.ksize, self.in_width), c]
+            max_num = float("-inf")
+            for i in range(x, min(x + self.ksize, self.in_height + 2*self.pad_h)):
+                for j in range(y : min(y + self.ksize, self.in_width + 2*self.pad_w)):
+                    if self.prev_res[b, i, j, c] > max_num:
+                        max_num = self.prev_res[b, i, j, c]
+            return max_num
 
         def multi(b, i, j, c):
             # m = get_max_from_window(b, i, j, c)
@@ -248,13 +251,13 @@ class BatchNorm(DnnNode):
         self.batch, self.in_height, self.in_width, self.in_channels = in_node.out_shape
 
         assert self.in_channels == mean.shape[0], \
-        f"Shape of biases must be equal to number of input channels, {mean.shape[0]} is not equal to {self.in_channels}"
+        "Shape of mean must be equal to number of input channels, %d is not equal to %d" % (mean.shape[0], self.in_channels)
 
         assert self.in_channels == variance.shape[0], \
-        f"Shape of variance must be equal to number of input channels, {variance.shape[0]} is not equal to {self.in_channels}"
+        "Shape of variance must be equal to number of input channels, %d is not equal to %d" % (variance.shape[0], self.in_channels)
 
         assert self.in_channels == gamma.shape[0], \
-        f"Shape of gamma must be equal to number of input channels, {gamma.shape[0]} is not equal to {self.in_channels}"
+        "Shape of gamma must be equal to number of input channels, %d is not equal to %d" % (gamma.shape[0], self.in_channels)
 
         # assert self.in_channels == epsilon.shape[0], \
         # f"Shape of epsilon must be equal to number of input channels, {epsilon.shape[0]} is not equal to {self.in_channels}"
@@ -266,16 +269,20 @@ class BatchNorm(DnnNode):
         self.epsilon = epsilon
         self.gamma = gamma
         self.variance = variance
+        self.result = np.copy(self.in_node.result)
+        self.out_shape = self.result.shape
 
     def run(self):
-        self.result = np.copy(self.in_node.result)#np.zeros(self.in_node.out_shape)
+        # self.result = np.copy(self.in_node.result)#np.zeros(self.in_node.out_shape)
+        def multi(b, h, w, c):
+            self.result[b, h, w, c] = \
+                (self.result[b, h, w, c] - self.mean[c]) / np.sqrt(self.variance[c] + self.epsilon)
         for b in range(self.batch):
             for h in range(self.in_height):
                 for w in range(self.in_width):
                     for c in range(self.in_channels):
-                        self.result[b, h, w, c] = 
+                        self.result[b, h, w, c] = \
                             (self.result[b, h, w, c] - self.mean[c]) / np.sqrt(self.variance[c] + self.epsilon)
-        self.out_shape = self.result.shape
 
 class LeakyReLU(DnnNode):
     def __init__(self, name, in_node):
